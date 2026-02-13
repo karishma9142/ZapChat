e3732da9679a05c703fd2ffa2445ae8efadfae47
karishma9142 <karishmacair47050@gmail.com>
2026-02-14 01:34:14 +0530

    add sidebar componene

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import assets from "../assets/assets";
 
-function ChatContainer(){
+function ChatContainer({ selectedser, setSelectedUser }){
     return (
         <div>
-            <h1>chat container</h1>
+            <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
+                <img src={assets.profile_martin} alt="martin" className="w-8 rounded-full"/>
+                <p className="flex-1 text-lg text-white flex items-center gap-2">
+                    Martin Johnson
+                    <span className="h-2 w-2 bg-green-500 rounded-full"></span>
+                </p>
+                <img onClick={() => setSelectedUser(null)} src={assets.arrow_icon} alt="" className="md:hidden max-w-7"/>
+            </div>
         </div>
     )
 }
--- client/src/components/RigthSidebar.jsx
+++ client/src/components/RigthSidebar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function RightSidebar (){
     return (
         <div>
-
+            <h1>right side bar</h1>
         </div>
     )
 }
