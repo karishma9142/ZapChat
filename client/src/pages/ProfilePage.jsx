f70fa761e368e0e48cf7dc099aa7189cd065df32
karishma9142 <karishmacair47050@gmail.com>
2026-02-16 12:53:55 +0530

    add chat page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,9 +1,38 @@
-import React from "react";
-
+import React, { useState } from "react";
+import {useNavigate} from 'react-router-dom';
+import assets from '../assets/assets'
 function ProfilePage(){
+
+    const [selectedImg , setSelectedImg] = useState(null);
+    const navigate = useNavigate();
+    const [name , setName] = useState("Martin Johnson");
+    const [bio , setBio] = useState("Hi Everyone, I am Using QuickChat")
     return (
-        <div>
-            <h1>Profle page</h1>
+        <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
+            <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 
+            flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+                <form className="flex flex-col flex-1 gap-5 p-10">
+                    <h3 className="text-lg">Profile details</h3>
+                    <label htmlFor="avatar" className="flex items-center gap-3 cursor-pointer">
+                        <input onChange={(e) => setSelectedImg(e.target.files[0])}
+                        type="file" id="avatar" accept=".png , .jpg , .jpeg" hidden/>
+                        <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} alt="ProfilePic"
+                        className={`w-12 h-12 ${selectedImg && "rounded-full"}`}/>
+                        <p>upload profile image</p>
+                    </label>
+                    <input onChange={(e) => setName(e.target.value)} value={name}
+                    type="text" required placeholder="Your name" className="p-2 border border-gray-500 rounded-md
+                    focus:outline-none focus:ring-2 focus:ring-violet-500"/>
+
+                    <textarea onChange={(e) => setBio(e.target.value)} value={bio}
+                    placeholder="Write profile bio" required rows={4} className="p-2 border border-gray-500 
+                    rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"></textarea>
+
+                    <button type="submit" className="bg-linear-to-r from-purple-400 to-violet-600 text-white
+                    p-2 rounded-full text-lg cursor-pointer">Save</button>
+                </form>
+                <img src={assets.logo_icon} alt="" />
+            </div>
         </div>
     )
 }
